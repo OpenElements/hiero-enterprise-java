2a1a787ce7f78c8503b46eaf031b3dce170c0f47
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-02-23 11:17:00 -0700

    some checks
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hiero-enterprise-base/src/main/java/com/openelements/hiero/base/data/ContractEventInstance.java
+++ hiero-enterprise-base/src/main/java/com/openelements/hiero/base/data/ContractEventInstance.java
@@ -3,9 +3,24 @@ package com.openelements.hiero.base.data;
 import com.hedera.hashgraph.sdk.ContractId;
 import com.openelements.hiero.smartcontract.abi.model.AbiParameterType;
 import java.util.List;
+import java.util.Objects;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
-public record ContractEventInstance(ContractId contractId, String eventName, List<ParameterInstance> parameters) {
+public record ContractEventInstance(@NonNull ContractId contractId, @Nullable String eventName,
+                                    @NonNull List<ParameterInstance> parameters) {
 
-    public record ParameterInstance(String name, AbiParameterType type, byte[] value) {
+    public ContractEventInstance {
+        Objects.requireNonNull(contractId, "contractId must be provided");
+        Objects.requireNonNull(parameters, "parameters must be provided");
+    }
+
+    public record ParameterInstance(@NonNull String name, @NonNull AbiParameterType type, @NonNull byte[] value) {
+
+        public ParameterInstance {
+            Objects.requireNonNull(name, "name must be provided");
+            Objects.requireNonNull(type, "type must be provided");
+            Objects.requireNonNull(value, "value must be provided");
+        }
     }
 }
