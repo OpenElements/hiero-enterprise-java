7b6621fc1356ce1bd3d1095282bfe4b132c5b3b6
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2025-03-12 10:12:20 +0300

    Increase the Timeout Duration
    Signed-off-by: Ndacyayisenga-droid <ndacyayinoah@gmail.com>

--- hiero-enterprise-test/src/main/java/com/openelements/hiero/test/HieroTestUtils.java
+++ hiero-enterprise-test/src/main/java/com/openelements/hiero/test/HieroTestUtils.java
@@ -1,5 +1,12 @@
 package com.openelements.hiero.test;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hedera.hashgraph.sdk.Status;
 import com.hedera.hashgraph.sdk.TransactionId;
 import com.openelements.hiero.base.HieroException;
@@ -7,11 +14,6 @@ import com.openelements.hiero.base.mirrornode.MirrorNodeClient;
 import com.openelements.hiero.base.protocol.ProtocolLayerClient;
 import com.openelements.hiero.base.protocol.TransactionListener;
 import com.openelements.hiero.base.protocol.data.TransactionType;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.concurrent.atomic.AtomicReference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class HieroTestUtils implements Serializable {
 
@@ -60,7 +62,7 @@ public class HieroTestUtils implements Serializable {
                     throw new RuntimeException("Error in mirror node query!", e);
                 }
                 if (!done) {
-                    if (LocalDateTime.now().isAfter(start.plusSeconds(30))) {
+                    if (LocalDateTime.now().isAfter(start.plusSeconds(60))) {
                         throw new RuntimeException("Timeout waiting for transaction");
                     }
                     try {
