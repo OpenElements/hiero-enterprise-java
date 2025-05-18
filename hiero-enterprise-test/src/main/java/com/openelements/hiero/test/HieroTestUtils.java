12f073d2c46c1f1159102cdb5693dc63253cfab2
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2025-05-18 18:20:25 +0300

    Add more time
    Signed-off-by: Ndacyayisenga-droid <ndacyayinoah@gmail.com>

--- hiero-enterprise-test/src/main/java/com/openelements/hiero/test/HieroTestUtils.java
+++ hiero-enterprise-test/src/main/java/com/openelements/hiero/test/HieroTestUtils.java
@@ -62,7 +62,7 @@ public class HieroTestUtils implements Serializable {
                     throw new RuntimeException("Error in mirror node query!", e);
                 }
                 if (!done) {
-                    if (LocalDateTime.now().isAfter(start.plusSeconds(600))) {
+                    if (LocalDateTime.now().isAfter(start.plusSeconds(1000))) {
                         throw new RuntimeException("Timeout waiting for transaction");
                     }
                     try {
